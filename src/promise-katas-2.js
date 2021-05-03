074548eac3236e59793235c30698d8ca4626fe57
Dean Spooner <dean.spooner@me.com>
2021-05-03 21:41:57 +0100

    Completed all tests.

--- src/promise-katas-2.js
+++ src/promise-katas-2.js
@@ -25,17 +25,33 @@ fetch("joke", "question").then(<insert your callback function>)
 // 1 Create a function that uses the fetch function to make a request to the "food" URL and returns
 // the data - expected return value "Cheese" of type String
 
-const food = () => {};
+const food = async () => {
+    const response = await fetch('food');
+    return response.data;
+};
 
 // 2 Create a function that uses the fetch function to make a request to the "cats" URL and returns
 // a list of cats in alphabetical order - expected return value ["Bandit", "Berry", "Puss in boots", "Smokey"] of type Array
 
-const cat = () => {};
+const cat = async () => {
+    const response = await fetch('cats');
+    response.data.cats.sort();
+    return response.data.cats;
+};
 
 // 3 Create a function that uses the fetch function to make a request to the "dogs" URL and returns
 // the naughtiest dog - expected return value {name: "Mutley", naughty: 10} of type Object
 
-const dog = () => {};
+const dog = async () => {
+    const response = await fetch('dogs');
+    let naughtiest = 0;
+    for (let i = 0; i < response.data.dogs.length; i++) {
+        if (response.data.dogs[i].naughty > naughtiest) {
+            naughtiest = response.data.dogs[i]
+        }
+    }
+    return naughtiest;
+};
 
 // 4 Create a function that uses the fetch function to make requests to the "jokes" URL and returns
 // a joke object with the key of question and answer - expected return { 
@@ -47,7 +63,26 @@ const dog = () => {};
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise/all
 //
 
-const joke = () => {};
+// const joke = async () => {
+//     const answer = await fetch('jokes', 'answer');
+//     const question = await fetch('jokes', 'question');
+//     const thisJoke = {
+//         question: question,
+//         answer: answer
+//     };
+//     return { thisJoke };
+// };
+
+const joke = () => {
+    const thisJoke = { question: '', answer: '' };
+    fetch('jokes', 'question').then((response) => {
+        thisJoke.question = response.joke;
+    });
+    fetch('jokes', 'answer').then((response) => {
+        thisJoke.answer = response.answer;
+    });
+    return thisJoke;
+};
 
 module.exports = {
     food,
